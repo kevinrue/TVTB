083faaa662c78c89b38fd4a3b8f8dd0efb4a9482
Kevin Rue-Albrecht <kevin.rue@ucdconnect.ie>
2016-11-04 10:47:00 +0000

    another attempt to ignore sections of code from codecov
    From: Kevin Rue-Albrecht <kevinrue67@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TVTB@123592 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tSVE.R
+++ R/tSVE.R
@@ -1,10 +1,10 @@
 # running the shiny web app.
 
-tSVE <- function(...){
+tSVE <- function(...){ # nocov start
     if (requireNamespace("shiny", quietly=TRUE)){
         message("Starting the Shiny web app.")
         shiny::runApp(system.file("shinyApp", package = "TVTB"), ...)
     } else {
         stop("Package shiny not installed!")
     }
-}
+} # nocov end
