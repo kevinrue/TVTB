64a6fb5d71b3368be697fcbde080176578f6ee6f
Kevin Rue-Albrecht <kevin.rue@ucdconnect.ie>
2016-11-04 10:29:40 +0000

    Moved from Import to Depends
    From: Kevin Rue-Albrecht <kevinrue67@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TVTB@123380 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tSVEParam-class.R
+++ R/tSVEParam-class.R
@@ -330,37 +330,3 @@ setReplaceMethod(
         x
     }
 )
-
-# Show ----
-
-setMethod(
-    f = "show",
-    signature = "tSVEParam",
-    definition = function(object){
-        cat("Genotypes\n")
-        cat(sprintf(
-            "- Reference: \"%s\" {%s}\n",
-            names(object@genos)[1],
-            paste(object@genos[[1]], collapse = ", ")))
-        cat(sprintf(
-            "- Heterozygote: \"%s\" {%s}\n",
-            names(object@genos)[2],
-            paste(object@genos[[2]], collapse = ", ")))
-        cat(sprintf(
-            "- Alternative: \"%s\" {%s}\n",
-            names(object@genos)[3],
-            paste(object@genos[[3]], collapse = ", ")))
-        cat(sprintf(
-            "Alternate allele frequency: \"%s\"\n",
-            object@aaf))
-        cat(sprintf(
-            "Minor allele frequency: \"%s\"\n",
-            object@maf))
-        cat(sprintf(
-            "Ensembl Variant Effet Predictor: \"%s\"\n",
-            object@vep))
-        cat("Bioconductor parallel parameters\n")
-        cat("- ")
-        print(object@bp)
-    }
-)
